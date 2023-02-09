5685ab6275dd143d50f0011229adb6d8609ab56f
Chemjo <thechasejohns@gmail.com>
2023-02-09 16:49:26 -0500

    Challenge 5

--- Week1/controllerWeek1.js
+++ Week1/controllerWeek1.js
@@ -1,10 +1,19 @@
-//Javascript file for Chase's Week 1 Challenges
+//--------------------------------------Javascript file for Chase's Week 1 Challenges--------------------------------------
 
-//Challenge IV: Adding an item to a stored list
+//--------------------------------------Challenge IV: Adding an item to a stored list--------------------------------------
 
-let myList = [];
+let myList = [];    //Array declaration with empty assignment
 
-function add(item,myList){
+function add(item,myList){  //Function to add one item to the list via a push
     myList.push(item);
 }
 
+//--------------------------------------Challenge V: Printing all Items in an Alert--------------------------------------
+
+function printList(myList){ //Function to print the entire list
+    let printList = "List of Items:\n";
+    for(let item in myList){
+        printList.concat(item+"\n");
+    }
+    alert(printList);
+}
